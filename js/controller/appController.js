050b7220bb43c9b4949903a5d51af3cb2b808b0a
PangeaBogota <Pangeabogota@hotmail.com>
2018-03-22 23:27:50 -0500

    Diseño de controles

--- js/controller/appController.js
+++ js/controller/appController.js
@@ -1,6 +1,88 @@
 var app = angular.module("CalculosFinancieros", []);
 
 app.controller("appController", function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
+    $scope.data=[];
+    $scope.respuesta=[];
+    $scope.respuesta.banderaError=false;
+    $scope.respuesta.mensajeError="";
+    $scope.respuesta.banderaSuccess=false;
+    $scope.respuesta.mensajeSuccess="";
+    $scope.Calcular=function()
+    {
+        if($scope.validarDatosIngreso())
+        {
+            $scope.respuesta.banderaError=false;
+            $scope.respuesta.banderaSuccess=false;
+            $scope.variablesUso=[];
+            
+        }
+        else{
+            $scope.respuesta.banderaError=true;
+            $scope.respuesta.banderaSuccess=false;
+        }
+    }
+    $scope.validarDatosIngreso=function()
+    {
+        if  ($scope.data.porcentaje!=undefined)
+        {
+            if ($scope.data.porcentaje==0)
+            {
+                $scope.respuesta.mensajeError="El porcentaje no puede ser 0.";
+                return false;
+            }
+        }
+        else{
+            $scope.respuesta.mensajeError="Debe Digitar el porcentaje.";
+            return false;
+        }
+        if  ($scope.data.tipoPresente!=undefined)
+        {
+            if ($scope.data.tipoPresente=="")
+            {
+                $scope.respuesta.mensajeError="El Tipo Presente no puede estar vacio.";
+                return false;
+            }
+        }
+        else{
+            $scope.respuesta.mensajeError="Debe seleccionar el tipo presente.";
+            return false;
+        }
+        if  ($scope.data.formPagoPresente!=undefined)
+        {
+            if ($scope.data.formPagoPresente=="")
+            {
+                $scope.respuesta.mensajeError="El forma de pago presente no puede estar vacio.";
+                return false;
+            }
+        }
+        else{
+            $scope.respuesta.mensajeError="Debe seleccionar el forma de pago presente.";
+            return false;
+        }
+        
+        if  ($scope.data.tipoFuturo!=undefined)
+        {
+            if ($scope.data.tipoFuturo=="")
+            {
+                $scope.respuesta.mensajeError="El tipo futuro  no puede estar vacio.";
+                return false;
+            }
+        }
+        else{
+            $scope.respuesta.mensajeError="Debe seleccionar el tipo futuro.";
+            return false;
+        }
+        if  ($scope.data.formaPagoFuturo!=undefined)
+        {
+            if ($scope.data.formaPagoFuturo=="")
+            {
+                $scope.respuesta.mensajeError="El forma de pago futuro no puede estar vacio.";
+                return false;
+            }
+        }
+        else{
+            $scope.respuesta.mensajeError="Debe seleccionar la forma de pago futuro.";
+            return false;
+        }
+    }
 });
